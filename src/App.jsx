6b7bc37d70a489debb787860edf39c02b1c09151
Fakhrii Habibillah An Naufal <fha.naufal06@gmail.com>
2021-12-15 13:52:14 +0700

    init vite + tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+function App() {
+  return (
+    <div className="flex justify-center">
+    <h1 className="font-bold text-2xl text-green-900">
+      Hello World
+    </h1>
+  </div>
+  )
+}
+
+export default App
