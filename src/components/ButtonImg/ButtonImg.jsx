c7a0b85a1b9c6344bf2e64dddc68ecb310a0227a
Ivan Hegiansyah <hegiansyahivan016@gmail.com>
2021-12-25 02:57:48 +0800

    fixing buttonimg

--- src/components/ButtonImg/ButtonImg.jsx
+++ src/components/ButtonImg/ButtonImg.jsx
@@ -1,13 +1,10 @@
-import React from "react";
-import del from "../../assets/icon/delete.svg";
-
-export default function ButtonDelete({ text, className, onClick }) {
+export default function ButtonImg({ text, icon, alt, className, onClick }) {
   return (
     <button
       className={`inline-flex items-center justify-center w-full px-4 py-3 bg-dark-green hover:bg-dark-green-hover text-light-gray text-lg transition-colors rounded-md shadow-lg ${className}`}
       onClick={onClick}
     >
-      <img src={del} alt="delete" className="mr-2" />
+      <img src={icon} alt={alt} className="mr-2" />
       {text}
     </button>
   );
