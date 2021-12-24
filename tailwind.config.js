b0719a304938b35100b142c59f1ec1931c51ddae
Ivan Hegiansyah <hegiansyahivan016@gmail.com>
2021-12-24 18:25:51 +0800

    config custom styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,24 @@
 module.exports = {
-  content: ["./src/**/*.{js,jsx}"],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dark-green": {
+          DEFAULT: "#006D77",
+          hover: "#085860",
+        },
+        "light-green": {
+          DEFAULT: "#83C5BE",
+        },
+        "light-gray": {
+          DEFAULT: "#EDF6F9",
+        },
+      },
+      fontFamily: {
+        title: ["Rancho"],
+        main: ["Poppins"],
+      },
+    },
   },
   plugins: [],
-}
+};
