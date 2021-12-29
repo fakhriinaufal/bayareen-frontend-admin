67ac44625b2531913e3ec5c68598fe4ebd772033
Ivan Hegiansyah <hegiansyahivan016@gmail.com>
2021-12-29 21:54:44 +0800

    create dropdownimg component

---
+++ src/components/DropdownImg/DropdownImg.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import down from "../../assets/icon/down.svg";
+import DropdownList from "./DropdownList";
+
+export default function DropdownImg({
+  icon,
+  name,
+  onChange,
+  value,
+  list,
+  containerClassName,
+  labelClassName,
+  listClassName,
+}) {
+  const [showList, setShowList] = useState(false);
+
+  function changeHandler(data) {
+    onChange(data);
+    setShowList(false);
+  }
+
+  return (
+    <div className={`flex flex-col ${containerClassName}`}>
+      <label
+        className={`text-dark-green pl-1 pb-1 ${labelClassName}`}
+        htmlFor={name}
+      ></label>
+
+      <div
+        onClick={() => setShowList((prev) => !prev)}
+        className={`bg-light-gray p-[.6rem] rounded-md flex text-dark-green mb-[.1rem] ${
+          showList && "outline outline-2 outline-dark-green"
+        }`}
+      >
+        <img src={icon} alt="icon" className="flex-none mr-2" />
+        <p className="flex-grow">{value.text}</p>
+        <img src={down} alt="down" className="flex-none" />
+      </div>
+      {showList && (
+        <ul className="rounded border transition-all">
+          {list.map((li) => (
+            <DropdownList
+              className={listClassName}
+              key={li.val}
+              data={li}
+              onClick={changeHandler}
+            />
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
