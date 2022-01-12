9398ca59088739d62ad0c119e7c15813f5d5907f
Ivan Hegiansyah <hegiansyahivan016@gmail.com>
2022-01-12 20:07:42 +0800

    add props imgClassName

--- src/components/ButtonImg/ButtonImg.jsx
+++ src/components/ButtonImg/ButtonImg.jsx
@@ -1,10 +1,10 @@
-export default function ButtonImg({ text, icon, alt, className, onClick }) {
+export default function ButtonImg({ text, icon, alt, className, onClick, imgClassName }) {
   return (
     <button
       className={`inline-flex items-center justify-center w-full px-4 py-3 bg-dark-green hover:bg-dark-green-hover text-light-gray text-lg transition-colors rounded-md shadow-lg ${className}`}
       onClick={onClick}
     >
-      <img src={icon} alt={alt} className="mr-2" />
+      <img src={icon} alt={alt} className={`mr-2 ${imgClassName}`} />
       {text}
     </button>
   );
