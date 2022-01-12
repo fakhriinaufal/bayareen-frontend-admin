0fcc877bea784c95338086d30669f05454d0f527
Ivan Hegiansyah <hegiansyahivan016@gmail.com>
2022-01-12 20:08:02 +0800

    add buttonUpdate

--- src/components/Table/TableCheck.jsx
+++ src/components/Table/TableCheck.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import ButtonImg from "../ButtonImg/ButtonImg";
+import update from "../../assets/icon/update.svg";
+import { useNavigate } from "react-router-dom";
 
-export default function TableCheck({ onCheck }) {
+export default function TableCheck({ onCheck, data }) {
+  const navigate = useNavigate();
   return (
-    <td className="px-5 py-3">
-      <input onChange={onCheck} type="checkbox" />
-    </td>
+    <>
+      <td className="px-5 py-3">
+        <input onChange={onCheck} type="checkbox" />
+      </td>
+      <td>
+        <ButtonImg
+          alt={"update product"}
+          icon={update}
+          className={"w-fit text-base px-2 py-2 bg-blue-600 hover:bg-blue-700 "}
+          onClick={() => navigate("/update-product", { state: data })}
+          imgClassName={"mr-0"}
+        />
+      </td>
+    </>
   );
 }
