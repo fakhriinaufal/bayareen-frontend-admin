97664d2ae44327c8ce929c931d3f045331d31369
Ivan Hegiansyah <hegiansyahivan016@gmail.com>
2022-01-13 16:17:57 +0800

    create updateProducts hooks

---
+++ src/hooks/useUpdateProducts.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+import { useState } from "react";
+
+export default function useUpdateProducts() {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const updateProducts = (product) => {
+    setLoading(true);
+    axios
+      .patch(`http://localhost:8080/products/${product.id}`, product)
+      .then((res) => {
+        console.log(res);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setLoading(false);
+        setError(error);
+      });
+  };
+  return { updateProducts, loading, error };
+}
